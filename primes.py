427cbd40abfaf002d49fa7ea88f6797c21ed2840
sssandy <shenshida@baidu.com>
2016-02-19 12:41:55 +0800

    素数的计算.

---
+++ primes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    for n in primes():
+        if n < 1000:
+            print(n)
+        else:
+            break
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    '''
+    #匿名函数替换.
+    def test(x):
+        return x % n > 0
+    return test
+    '''
+    return lambda x: x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+if __name__ == '__main__':
+    main()
